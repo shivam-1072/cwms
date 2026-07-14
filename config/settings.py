371eb079a8cee79b523c11ab702d62b4f41be7a9
shivam-1072 <sshivam107201@gmail.com>
2026-07-14 13:44:40 +0000

    fix: Add static and media directories for collectstatic

--- config/settings.py
+++ config/settings.py
@@ -136,8 +136,8 @@ STATIC_ROOT = '/app/static'  # Where collectstatic will put files
 STATICFILES_DIRS = [BASE_DIR / 'static']        # No additional directories
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Security Settings
 LOGIN_URL = '/workers/login/'
